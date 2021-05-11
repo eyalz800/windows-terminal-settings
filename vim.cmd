1b387bc2f2745acd162232cc9a3713554af3e799
eyalz800 <eyalz800@gmail.com>
2021-05-12 02:12:49 +0300

    Removed prebuilt terminal, the terminal from windows store contains necessary features, added CascadiaMonoPL font.

--- vim.cmd
+++ vim.cmd
@@ -1,1 +1,1 @@
-@C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\wtd.exe bash -c "echo '%*' | sed 's@\\@/@g' | sed 's@C:@/mnt/c@g' | xargs -o vim"
+@C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\wt.exe bash -c "echo '%*' | sed 's@\\@/@g' | sed 's@C:@/mnt/c@g' | xargs -o vim"
