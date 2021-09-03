2bbd69a2afd8e7c6c8495d999207c155e14e2f28
eyalz800 <eyalz800@gmail.com>
2021-09-03 17:07:34 +0300

    Updated after some behavior changes to windows.

--- vim.cmd
+++ vim.cmd
@@ -1,1 +1,1 @@
-@C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\wt.exe bash -c "echo '%*' | sed 's@\\@/@g' | sed 's@C:@/mnt/c@g' | xargs -o vim"
+@C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\Microsoft.WindowsTerminal_8wekyb3d8bbwe\wt.exe bash -c "echo '%*' | sed 's@\\\\@/@g' | sed 's@C:@/mnt/c@g' | xargs -o vim"
